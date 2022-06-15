a4cd91be8ef3fc51515002f8535afcc83fb6ae1c
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2022-06-15 18:37:58 -0400

    update

--- build/lib/DPSDV/tabular/__init__.py
+++ build/lib/DPSDV/tabular/__init__.py
@@ -3,10 +3,12 @@
 from DPSDV.tabular.copulagan import CopulaGAN
 from DPSDV.tabular.copulas import GaussianCopula
 from DPSDV.tabular.ctgan import CTGAN, TVAE
+from DPSDV.tabular.mwem import MWEMSynthesizer
 
 __all__ = (
     'GaussianCopula',
     'CTGAN',
     'TVAE',
     'CopulaGAN',
+    'MWEMSynthesizer',
 )
