89cb3067382c71c1d63aaadd63e21be683aa6c3c
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2022-06-15 18:33:13 -0400

    mwem

--- demo_single.py
+++ demo_single.py
@@ -1,6 +1,6 @@
 from DPSDV.demo import load_tabular_demo
 from DPSDV.lite import TabularPreset
-from DPSDV.tabular import GaussianCopula, CopulaGAN, CTGAN, TVAE
+from DPSDV.tabular import GaussianCopula, CopulaGAN, CTGAN, TVAE, MWEMSynthesizer
 from DPSDV.evaluation import evaluate
 from DPSDV.metrics.tabular import BNLikelihood, BNLogLikelihood, GMLogLikelihood, LogisticDetection, SVCDetection, NumericalLR, NumericalMLP, NumericalSVR
 import warnings
@@ -16,12 +16,24 @@ def compute_results(synthetic_data, data):
 	metrics_arr = [BNLikelihood, BNLogLikelihood, GMLogLikelihood]
 	metrics_arr_ = [LogisticDetection, SVCDetection]
 	for m in metrics_arr:
-		p.update({m.__name__: m.compute(data.fillna(0), synthetic_data.fillna(0))})
+		try:
+			p.update({m.__name__: m.compute(data.fillna(0), synthetic_data.fillna(0))})
+		except:
+			p.update({m.__name__: None})
 	for m in metrics_arr_:
-		p.update({m.__name__: m.compute(data, synthetic_data)})
+		try:
+			p.update({m.__name__: m.compute(data, synthetic_data)})
+		except:
+			p.update({m.__name__: None})
 	metrics_arr__ = [NumericalLR, NumericalMLP, NumericalSVR]
 	for m in metrics_arr__:
-		p.update({'PRIV_METRIC_'+m.__name__: m.compute(data.fillna(0),synthetic_data.fillna(0),key_fields=['second_perc', 'mba_perc', 'degree_perc'],sensitive_fields=['salary'])})
+		try:
+			p.update({'PRIV_METRIC_'+m.__name__: m.compute(data.fillna(0),synthetic_data.fillna(0),key_fields=['second_perc', 'mba_perc', 'degree_perc'],sensitive_fields=['salary'])})
+		except:
+			try:
+				p.update({'PRIV_METRIC_'+m.__name__: m.compute(data.fillna(0),synthetic_data.fillna(0),key_fields=['age', 'sex', 'educ', 'race'],sensitive_fields=['married'])})
+			except:
+				p.update({'PRIV_METRIC_'+m.__name__: None})
 	return p
 
 performance = []
@@ -44,6 +56,19 @@ r = {'name':'FAST_ML'}
 r.update(compute_results(synthetic_data, data))
 performance.append(r)
 synthetic_data.to_csv("./single_demo_results/FAST_ML_normal.csv", index=False)
+
+
+df = pd.read_csv("./DPSDV/data/PUMS_california_demographics/data.csv")
+df = df.drop(["income"], axis=1)
+print('-'*40,'\n')
+model = MWEMSynthesizer(epsilon=1.0)
+model.fit(df)
+synthetic_data = model.sample(num_rows=100)
+print('MWEM-DP')
+r = {'name':'MWEM-DP'}
+r.update(compute_results(synthetic_data, df))
+performance.append(r)
+synthetic_data.to_csv("./single_demo_results/MWEM_dp.csv", index=False)
 
 print('-'*40, '\n')
 model = GaussianCopula()
